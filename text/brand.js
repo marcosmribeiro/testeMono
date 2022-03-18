3a9405dc64c7c74430779165326ed294dc0a9359
Marcos Ribeiro <ribeiro.marcos17@gmail.com>
2022-03-17 23:49:21 -0300

    Commit de teste

---
+++ text/brand.js
@@ -0,0 +1,16 @@
+const brand = {
+  mono: {
+    name: 'Mono - No Aware',
+    desc: 'restaurante Mono - Non Aware',
+    prefix: 'mono',
+    footerText: 'MONO @2022',
+    logoText: 'Mono - No Aware',
+    projectName: 'Mono',
+    url: 'restaurantemono.com.br',
+    img: '/static/images/logotipo-topo.png',
+    notifMsg:
+      'Donec sit amet nulla sed arcu pulvinar ultricies commodo id ligula.'
+  }
+}
+
+export default brand
