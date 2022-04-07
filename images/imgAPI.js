21a337d5e5e5d7826b506ab9d40871861dc0d58b
Marcos Ribeiro <ribeiro.marcos17@gmail.com>
2022-04-07 00:05:17 -0300

    Atualizando

--- images/imgAPI.js
+++ images/imgAPI.js
@@ -1,5 +1,13 @@
 const imgAPI = {
   instagram: [
+    { img: 'https://scontent.cdninstagram.com/v/t51.29350-15/277885379_1129572434558482_4965181606823836713_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=8ae9d6&_nc_ohc=NfoFOSxBoIUAX-FM3ay&_nc_ht=scontent.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AT_hzhv-gTWXBu6Qz4699v0iVU39Oy2pKaSfmKEsrAN9OQ&oe=625377FA', link: "https://www.instagram.com/p/CXwzz97quLh/" },
+    { img: 'https://scontent.cdninstagram.com/v/t51.29350-15/277856962_398782865419265_4075979745728022756_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=8ae9d6&_nc_ohc=XEjwbQRGkUQAX8Em_YK&_nc_ht=scontent.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AT9XRsTN_MHzGc0i-X29gKdB1S7LsvkqvDeHXFJMiAYINw&oe=6253DA65', link: "https://www.instagram.com/p/CXwzz97quLh/" },
+    { img: 'https://scontent.cdninstagram.com/v/t51.29350-15/277904828_511142550610445_932386956560453275_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=8ae9d6&_nc_ohc=FdHSDIvxZzYAX-HFtGJ&_nc_oc=AQkWFtbP2QThEFqPjLY7U_M7ki6xrgMjwB2n1ouelhyNTvz6Mrqr2i6pQLfAiNGttdg&_nc_ht=scontent.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AT9b8S_X_JQGWvVoOVVe4BPM9aqiFWpI5N9TjGJG0s7YvA&oe=62541636', link: "https://www.instagram.com/p/CXwzz97quLh/" },
+    { img: 'https://scontent.cdninstagram.com/v/t51.29350-15/277595839_359408945946184_8943323159675565336_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=8ae9d6&_nc_ohc=nC-SNTreV4sAX8OsSVu&_nc_ht=scontent.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AT8Vs300tUQ3e7WtVT23UKx84meETr-DsKMO9GDvl5AlKA&oe=62535C8D', link: "https://www.instagram.com/p/CXwzz97quLh/" },
+    { img: 'https://scontent.cdninstagram.com/v/t51.29350-15/277417805_536681937875262_8186439244113927348_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=8ae9d6&_nc_ohc=wNAs17KdGkkAX_7PgIk&_nc_ht=scontent.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AT88s8-QyVdmGT2X91Ca03U3rLeRvhTSfbFA4AGfWDsimg&oe=6253FEB7', link: "https://www.instagram.com/p/CXwzz97quLh/" },
+    { img: 'https://scontent.cdninstagram.com/v/t51.29350-15/277605513_1196320281174386_1428375799208390939_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=8ae9d6&_nc_ohc=YJyWOQ_RGXwAX84AFE3&_nc_ht=scontent.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AT8jwBWADC0-S4KOdxcEROxb5IkoYB1nzpZ67UsBioEVeA&oe=6252A048', link: "https://www.instagram.com/p/CXwzz97quLh/" }
+  ],
+  instagramOLD: [
     {img: '/images/mono/Barriga_de_Salmão.jpg', link: "https://www.instagram.com/p/CXwzz97quLh/" },
     {img: '/images/mono/Carapau.jpg', link: "https://www.instagram.com/p/CXwzz97quLh/" },
     {img: '/images/mono/Gyoza_de_Copa_Lombo.jpg', link: "https://www.instagram.com/p/CXwzz97quLh/" },
